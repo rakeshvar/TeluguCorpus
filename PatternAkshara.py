32ebe6454197223dd8f0b9ab3e3b197192aa9c0c
rakeshvar <rakeshvar@gmail.com>
2025-09-04 00:00:22 -0400

    Akshara Pattern Perfected!

--- PatternAkshara.py
+++ PatternAkshara.py
@@ -12,14 +12,15 @@ O = '[ॐ।॥₹\u200C]'                # Others
 P = '[!()+,./0-9:;=?]'           # Common Punctuation
 Q = r'[#$%&*<>@[\\\]^_`{|}~]'    # Uncommon Punctuation
 X = '["\'`-\t\r]'                # Should be replaced with Unicode equivalent
-R = '[–—‘’“”]'                   # Unicode Punctuation
+R = '[-—‘’“”]'                   # Unicode Punctuation
 S = r'[ \n]'                     # Space
 E = '[a-zA-Z]'                   # English
+Z = '[ౘౙఽ]'
 
-akshara = (f"{U}|{A}|{N}|{P}|{O}|{S}|{E}|{Q}|"        # Individuals
-           f"{H}(?:{V}{H})*{M}|"                      # Compounds with matra
-           f"(?:{H}{V})+(?!{H})|"                     # Compounds with virama
-           f"{H}(?:{V}{H})*(?!{MV})")                 # Compounds without matra
+akshara = (f"{U}|{A}|{N}|{O}|{P}|{Q}|{R}|{S}|{E}|{Z}|" # Individuals
+           f"{H}(?:{V}{H})*{M}|"                       # Compounds with matra
+           f"(?:{H}{V})+(?!{H})|"                      # Compounds with virama
+           f"{H}(?:{V}{H})*(?!{MV})")                  # Compounds without matra
 
 print("Akshara Pattern: ", akshara)
 akshara_pattern = re.compile(akshara)
